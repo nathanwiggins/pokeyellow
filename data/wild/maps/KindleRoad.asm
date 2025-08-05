f49014bb49463807f5dd952a27bc64ea9511d274
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-05 15:08:25 -0600

    Update KindleRoad.asm

--- data/wild/maps/KindleRoad.asm
+++ data/wild/maps/KindleRoad.asm
@@ -1,15 +1,5 @@
 KindleRoadWildMons:
-	def_grass_wildmons 25 ; encounter rate
-	db 15, PIDGEY
-	db 13, RATTATA
-	db 13, PIDGEY
-	db 11, PIDGEY
-	db 17, PIDGEY
-	db 15, RATTATA
-	db 15, RATICATE
-	db 17, PIDGEOTTO
-	db 19, PIDGEOTTO
-	db 15, PIDGEOTTO
+	def_grass_wildmons 0 ; encounter rate
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
