837531f162baf1558813342ca06ea3f351055513
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-05 15:04:33 -0600

    Update Route21.asm

--- data/maps/headers/Route21.asm
+++ data/maps/headers/Route21.asm
@@ -1,5 +1,5 @@
 	map_header Route21, ROUTE_21, OVERWORLD, NORTH | SOUTH | WEST
 	connection north, PalletTown, PALLET_TOWN, 0
 	connection south, CinnabarIsland, CINNABAR_ISLAND, 0
-	connection west, KindleRoad, KINDLE_ROAD, 0
+	connection west, KindleRoad, KINDLE_ROAD, 5
 	end_map_header
