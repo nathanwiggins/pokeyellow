217f3537e2d3a6e6752420f733c64e490ddcf900
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-11 22:20:27 -0400

    Create MtEmber1F.asm

---
+++ data/wild/maps/MtEmber1F.asm
@@ -0,0 +1,16 @@
+MtEmber1FWildMons:
+	def_grass_wildmons 10 ; encounter rate
+	db  8, CHARMANDER
+	db  7, CHARMANDER
+	db  9, CHARMANDER
+	db  8, CHARMANDER
+	db  6, CHARMANDER
+	db 10, CHARMANDER
+	db 10, CHARMANDER
+	db  8, CHARMANDER
+	db 11, CHARMANDER
+	db  8, CHARMANDER
+	end_grass_wildmons
+
+	def_water_wildmons 0 ; encounter rate
+	end_water_wildmons
