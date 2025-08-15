28492c9fed7f516989cb9ce58793dcaebb9b65eb
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-15 14:34:35 -0600

    Create WildMeadow.asm

---
+++ data/wild/maps/WildMeadow.asm
@@ -0,0 +1,16 @@
+WildMeadowWildMons:
+	def_grass_wildmons 10 ; encounter rate
+	db 20, VENUSAUR
+	db 21, VENUSAUR
+	db 22, VENUSAUR
+	db 23, VENUSAUR
+	db 24, VENUSAUR
+	db 19, VENUSAUR
+	db 18, VENUSAUR
+	db 25, VENUSAUR
+	db 20, VENUSAUR
+	db 25, VENUSAUR
+	end_grass_wildmons
+
+	def_water_wildmons 0 ; encounter rate
+	end_water_wildmons
