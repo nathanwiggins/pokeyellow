62095754ec56f95722b3748a9359c4ddc3b0f90f
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-16 09:53:11 -0600

    Update WildMeadow.asm

--- data/maps/objects/WildMeadow.asm
+++ data/maps/objects/WildMeadow.asm
@@ -1,3 +1,8 @@
+        object_const_def
+		const_export WILDMEADOW_MASTER_BALL
+		const_export WILDMEADOW_NUGGET
+		const_export WILDMEADOW_TM15
+
 WildMeadow_Object:
 	db $02 ; border block
 
@@ -7,5 +12,8 @@ WildMeadow_Object:
 	def_bg_events
 
 	def_object_events
+    object_event 16, 6, SPRITE_POKE_BALL, STAY, NONE, TEXT_WILDMEADOW_MASTER_BALL, MASTER_BALL
+    object_event 26, 3, SPRITE_POKE_BALL, STAY, NONE, TEXT_WILDMEADOW_NUGGET, NUGGET
+    object_event 11, 27, SPRITE_POKE_BALL, STAY, NONE, TEXT_WILDMEADOW_TM15, TM_HYPER_BEAM
 
 	def_warps_to WILD_MEADOW
