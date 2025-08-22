59203f2cb0c146101e7853e86269567aaecc4bb0
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-21 19:12:20 -0600

    Create WaterPath.asm

---
+++ data/wild/maps/WaterPath.asm
@@ -0,0 +1,16 @@
+WaterPathWildMons:
+	def_grass_wildmons 0 ; encounter rate
+	end_grass_wildmons
+
+	def_water_wildmons 5 ; encounter rate
+	db  5, BLASTOISE
+	db 10, BLASTOISE
+	db 15, BLASTOISE
+	db  5, BLASTOISE
+	db 10, BLASTOISE
+	db 15, BLASTOISE
+	db 20, BLASTOISE
+	db 30, BLASTOISE
+	db 35, BLASTOISE
+	db 40, BLASTOISE
+	end_water_wildmons
