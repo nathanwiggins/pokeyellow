1d5cd1d887664cfe306017d0f1bfa846a18a863f
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-21 18:58:36 -0600

    Update WaterPath.asm

--- data/maps/headers/WaterPath.asm
+++ data/maps/headers/WaterPath.asm
@@ -1,3 +1,3 @@
-	map_header WaterPath, WATER_PATH, OVERWORLD, WEST
-	connection west, WildMeadow, WILD_MEADOW, 0
+	map_header WaterPath, WATER_PATH, OVERWORLD, EAST
+	connection east, WildMeadow, WILD_MEADOW, 0
 	end_map_header
