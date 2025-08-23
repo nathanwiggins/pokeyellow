a947031f5cf59162ce68c0838f5ab773fbf82d2e
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-23 13:51:17 -0600

    Update RuinValley.asm

--- data/maps/headers/RuinValley.asm
+++ data/maps/headers/RuinValley.asm
@@ -1,3 +1,3 @@
-	map_header WaterPath, WATER_PATH, OVERWORLD, EAST
-  connection east, WaterPath, WATER_PATH, 0
+	map_header RuinValley, RUIN_VALLEY, OVERWORLD, EAST
+  	connection east, WaterPath, WATER_PATH, 0
 	end_map_header
