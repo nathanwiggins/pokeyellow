2160dafa3e44dc9804545aa3123e80a54ec8da91
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-24 13:03:43 -0600

    Create PokemonTemple.asm

---
+++ data/maps/objects/PokemonTemple.asm
@@ -0,0 +1,16 @@
+    object_const_def
+    const_export POKEMONTEMPLE_MEW
+
+PokemonTemple_Object:
+	db $01 ; border block
+
+	def_warp_events
+	warp_event 10, 17, LAST_MAP, 1
+  warp_event 11, 17, LAST_MAP, 1
+
+	def_bg_events
+
+	def_object_events
+	object_event 7, 5, SPRITE_MONSTER, STAY, UP, TEXT_POKEMONTEMPLE_MEW, MEW, 30
+
+	def_warps_to POKEMON_TEMPLE
