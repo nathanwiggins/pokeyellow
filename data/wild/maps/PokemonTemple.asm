f00881f73fdf58c85ec6b8a5bd234419e8b032df
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-24 13:07:44 -0600

    Create PokemonTemple.asm

---
+++ data/wild/maps/PokemonTemple.asm
@@ -0,0 +1,16 @@
+PokemonTempleWildMons:
+	def_grass_wildmons 10 ; encounter rate
+	db  8, MEW
+	db  7, MEW
+	db  9, MEW
+	db  8, MEW
+	db  6, MEW
+	db 10, MEW
+	db 10, MEW
+	db  8, MEW
+	db 11, MEW
+	db  8, MEW
+	end_grass_wildmons
+
+	def_water_wildmons 0 ; encounter rate
+	end_water_wildmons
