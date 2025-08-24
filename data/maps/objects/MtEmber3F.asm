005637827ee34efdb854e966a9709ce2986fd4e6
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-24 13:59:13 -0600

    Update MtEmber3F.asm

--- data/maps/objects/MtEmber3F.asm
+++ data/maps/objects/MtEmber3F.asm
@@ -10,6 +10,6 @@ MtEmber3F_Object:
 	def_bg_events
 
 	def_object_events
-	object_event 7, 5, SPRITE_BIRD, STAY, UP, TEXT_MTEMBER3F_MOLTRES, MOLTRES, 50
+	object_event 7, 5, SPRITE_BIRD, STAY, DOWN, TEXT_MTEMBER3F_MOLTRES, MOLTRES, 50
 
 	def_warps_to MT_EMBER_3F
