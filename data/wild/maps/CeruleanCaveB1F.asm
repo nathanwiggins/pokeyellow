a2f44c21a679d10d607b766bb6b45970ccffa07c
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-27 15:55:07 -0600

    Update CeruleanCaveB1F.asm

--- data/wild/maps/CeruleanCaveB1F.asm
+++ data/wild/maps/CeruleanCaveB1F.asm
@@ -12,5 +12,14 @@ CeruleanCaveB1FWildMons:
 	db  70, MEWTWO
 	end_grass_wildmons
 
+; Yellow Version:
+; NIDOKING (15%)
+; NIDOQUEEN (15%)
+; ALAKAZAM (30%)
+; GENGAR (30%)
+; AERODACTYL (5%)
+; DRAGONITE (4%)
+; MEWTWO (1%)
+
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
