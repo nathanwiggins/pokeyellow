26edc09d2286413b421491e26f4ac272c388f336
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-27 15:52:33 -0600

    Update CeruleanCave1F.asm

--- data/wild/maps/CeruleanCave1F.asm
+++ data/wild/maps/CeruleanCave1F.asm
@@ -1,15 +1,15 @@
 CeruleanCave1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 50, GOLBAT
-	db 55, GOLBAT
-	db 45, GRAVELER
-	db 55, GLOOM
-	db 55, WEEPINBELL
-	db 52, SANDSLASH
-	db 54, VENOMOTH
-	db 54, PARASECT
-	db 55, DITTO
-	db 60, DITTO
+	db  60, NIDOKING
+	db  60, NIDOQUEEN
+	db  50, ALAKAZAM
+	db  50, ALAKAZAM
+	db  50, GENGAR
+	db  50, GENGAR
+	db  50, GENGAR
+	db  60, AERODACTYL
+	db  65, DRAGONITE
+	db  65, DRAGONITE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
