588d388c2dceed6db82b9fe99e8070997792ca30
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-27 15:55:33 -0600

    Update CeruleanCave2F.asm

--- data/wild/maps/CeruleanCave2F.asm
+++ data/wild/maps/CeruleanCave2F.asm
@@ -12,5 +12,13 @@ CeruleanCave2FWildMons:
 	db  65, DRAGONITE
 	end_grass_wildmons
 
+; Yellow Version:
+; NIDOKING (15%)
+; NIDOQUEEN (15%)
+; ALAKAZAM (30%)
+; GENGAR (30%)
+; AERODACTYL (5%)
+; DRAGONITE (5%)
+
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
