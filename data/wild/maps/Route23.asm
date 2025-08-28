afb02a60666d4f39cc2bbeb21270af3e971b8f72
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-28 16:11:40 -0600

    Update Route23.asm

--- data/wild/maps/Route23.asm
+++ data/wild/maps/Route23.asm
@@ -1,16 +1,25 @@
 Route23WildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 41, NIDORINO
-	db 41, NIDORINA
-	db 36, MANKEY
-	db 44, NIDORINO
-	db 44, NIDORINA
-	db 40, FEAROW
-	db 41, MANKEY
-	db 45, FEAROW
-	db 41, PRIMEAPE
-	db 46, PRIMEAPE
+	db  50, PIDGEOT
+	db  45, PRIMEAPE
+	db  45, SANDSLASH
+	db  45, ARBOK
+	db  45, SANDSLASH
+	db  45, ARBOK
+	db  60, EXEGGUTOR
+	db  45, SCYTHER
+	db  45, PINSIR
+	db  45, PINSIR
 	end_grass_wildmons
+
+; Yellow Version:
+; PIDGEOT (15%)
+; PRIMEAPE (15%)
+; SANDSLASH (25%)
+; ARBOK (25%)
+; EXEGGUTOR (10%)
+; SCYTHER (5%)
+; PINSIR (5%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
