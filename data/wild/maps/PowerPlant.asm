ae069a2e0ba02eba5edc5a2e31bb59eb6e03fd5e
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-28 16:23:00 -0600

    Update PowerPlant.asm

--- data/wild/maps/PowerPlant.asm
+++ data/wild/maps/PowerPlant.asm
@@ -1,16 +1,24 @@
 PowerPlantWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 30, MAGNEMITE
-	db 35, MAGNEMITE
-	db 33, MAGNETON
-	db 33, VOLTORB
-	db 37, VOLTORB
-	db 33, GRIMER
-	db 37, GRIMER
-	db 38, MAGNETON
-	db 33, MUK
-	db 37, MUK
+	db  45, MAGNETON
+	db  45, MAGNETON
+	db  45, ELECTRODE
+	db  45, ELECTRODE
+	db  45, MAGNETON
+	db  45, ELECTRODE
+	db  45, ELECTABUZZ
+	db  45, PORYGON
+	db  60, RAICHU
+	db  50, ZAPDOS
 	end_grass_wildmons
+
+; Yellow Version:
+; MAGNETON (40%)
+; ELECTRODE (40%)
+; ELECTABUZZ (10%)
+; PORYGON (5%)
+; RAICHU (4%)
+; ZAPDOS (1%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
