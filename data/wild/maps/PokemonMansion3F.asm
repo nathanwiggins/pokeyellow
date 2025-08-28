6770f062c34be891b470eb470a0407083fceacde
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-28 16:26:17 -0600

    Update PokemonMansion3F.asm

--- data/wild/maps/PokemonMansion3F.asm
+++ data/wild/maps/PokemonMansion3F.asm
@@ -1,16 +1,24 @@
 PokemonMansion3FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 40, RATTATA
-	db 40, RATICATE
-	db 32, GRIMER
-	db 35, GRIMER
-	db 43, RATTATA
-	db 43, RATICATE
-	db 38, GRIMER
-	db 38, GRIMER
-	db 38, MUK
-	db 41, MUK
+	db  45, MUK
+	db  45, MUK
+	db  45, WEEZING
+	db  45, WEEZING
+	db  45, HYPNO
+	db  45, HYPNO
+	db  45, MR_MIME
+	db  50, HITMONLEE
+	db  50, HITMONCHAN
+	db  50, HITMONCHAN
 	end_grass_wildmons
+
+; Yellow Version:
+; MUK (30%)
+; WEEZING (30%)
+; HYPNO (20%)
+; MR_MIME (10%)
+; HITMONLEE (5%)
+; HITMONCHAN (5%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
