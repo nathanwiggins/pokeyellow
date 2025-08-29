6908939aab083ba9bd3601fd819387c6bbecab22
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-29 14:26:48 -0600

    Update Route15.asm

--- data/wild/maps/Route15.asm
+++ data/wild/maps/Route15.asm
@@ -1,16 +1,22 @@
 Route15WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 26, ODDISH
-	db 26, BELLSPROUT
-	db 24, VENONAT
-	db 32, PIDGEOTTO
-	db 28, ODDISH
-	db 28, BELLSPROUT
-	db 30, GLOOM
-	db 30, WEEPINBELL
-	db 27, VENONAT
-	db 30, VENOMOTH
+	db  45, BUTTERFREE
+	db  45, BUTTERFREE
+	db  45, BEEDRILL
+	db  45, BEEDRILL
+	db  45, VENOMOTH
+	db  45, VENOMOTH
+	db  45, VENOMOTH
+	db  45, CHANSEY
+	db  45, CHANSEY
+	db  45, CHANSEY
 	end_grass_wildmons
+
+; Yellow Version:
+; BUTTERFREE (30%)
+; BEEDRILL (30%)
+; VENOMOTH (30%)
+; CHANSEY (10%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
