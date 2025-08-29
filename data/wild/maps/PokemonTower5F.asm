dfe1ed15e2352978743d49ab6111e81e9fe91f55
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-29 14:39:01 -0600

    Update PokemonTower5F.asm

--- data/wild/maps/PokemonTower5F.asm
+++ data/wild/maps/PokemonTower5F.asm
@@ -1,16 +1,22 @@
 PokemonTower5FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 22, GASTLY
-	db 23, GASTLY
-	db 24, GASTLY
-	db 25, GASTLY
-	db 26, GASTLY
-	db 21, GASTLY
-	db 20, CUBONE
-	db 27, GASTLY
-	db 22, HAUNTER
-	db 27, HAUNTER
+	db  10, GASTLY
+	db  10, GASTLY
+	db  10, GASTLY
+	db  10, GASTLY
+	db  25, HAUNTER
+	db  25, HAUNTER
+	db  25, KADABRA
+	db  25, DITTO
+	db  25, DITTO
+	db  25, DITTO
 	end_grass_wildmons
+
+; Yellow Version:
+; GASTLY (60%)
+; HAUNTER (20%)
+; KADABRA (10%)
+; DITTO (10%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
