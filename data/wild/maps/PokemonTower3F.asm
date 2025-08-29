b2ec0384ada851415b1f595e0fb2e83dc62694d1
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-29 14:38:44 -0600

    Update PokemonTower3F.asm

--- data/wild/maps/PokemonTower3F.asm
+++ data/wild/maps/PokemonTower3F.asm
@@ -1,16 +1,22 @@
 PokemonTower3FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 20, GASTLY
-	db 21, GASTLY
-	db 22, GASTLY
-	db 23, GASTLY
-	db 24, GASTLY
-	db 19, GASTLY
-	db 18, GASTLY
-	db 25, GASTLY
-	db 20, HAUNTER
-	db 25, HAUNTER
+	db  10, GASTLY
+	db  10, GASTLY
+	db  10, GASTLY
+	db  10, GASTLY
+	db  25, HAUNTER
+	db  25, HAUNTER
+	db  25, KADABRA
+	db  25, DITTO
+	db  25, DITTO
+	db  25, DITTO
 	end_grass_wildmons
+
+; Yellow Version:
+; GASTLY (60%)
+; HAUNTER (20%)
+; KADABRA (10%)
+; DITTO (10%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
