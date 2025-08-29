895508bf10ef853ed2fb1843e4be01e30446d865
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-29 14:31:14 -0600

    Update Route13.asm

--- data/wild/maps/Route13.asm
+++ data/wild/maps/Route13.asm
@@ -1,16 +1,22 @@
 Route13WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 25, ODDISH
-	db 25, BELLSPROUT
-	db 28, PIDGEOTTO
-	db 28, PIDGEY
-	db 27, ODDISH
-	db 27, BELLSPROUT
-	db 29, GLOOM
-	db 29, WEEPINBELL
-	db 26, FARFETCHD
-	db 31, FARFETCHD
+	db  30, GLOOM
+	db  30, GLOOM
+	db  30, WEEPINBELL
+	db  30, WEEPINBELL
+	db  30, POLIWHIRL
+	db  30, POLIWHIRL
+	db  30, POLIWHIRL
+	db  45, LICKITUNG
+	db  45, LICKITUNG
+	db  45, LICKITUNG
 	end_grass_wildmons
+
+; Yellow Version:
+; GLOOM (30%)
+; WEEPINBELL (30%)
+; POLIWHIRL (30%)
+; LICKITUNG (10%)
 
 	def_water_wildmons 3 ; encounter rate
 	db  10, TENTACOOL
