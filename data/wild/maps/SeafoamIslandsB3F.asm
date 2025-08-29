dba0b3e396850836eb4bae4eaa0f164aac30a880
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-29 14:11:52 -0600

    Update SeafoamIslandsB3F.asm

--- data/wild/maps/SeafoamIslandsB3F.asm
+++ data/wild/maps/SeafoamIslandsB3F.asm
@@ -1,16 +1,25 @@
 SeafoamIslandsB3FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 27, GOLBAT
-	db 36, ZUBAT
-	db 29, KRABBY
-	db 27, ZUBAT
-	db 30, KINGLER
-	db 26, SEEL
-	db 31, KRABBY
-	db 30, SEEL
-	db 28, DEWGONG
-	db 32, DEWGONG
+	db  45, GOLDUCK
+	db  45, GOLDUCK
+	db  45, SLOWBRO
+	db  45, SLOWBRO
+	db  45, JYNX
+	db  45, DEWGONG
+	db  45, KINGLER
+	db  60, CLOYSTER
+	db  30, WARTORTLE
+	db  30, WARTORTLE
 	end_grass_wildmons
+
+; Yellow Version:
+; GOLDUCK (30%)
+; SLOWBRO (30%)
+; JYNX (10%)
+; DEWGONG (10%)
+; KINGLER (10%)
+; CLOYSTER (5%)
+; WARTORTLE (5%)
 
 	def_water_wildmons 5 ; encounter rate
 	db 25, TENTACOOL
