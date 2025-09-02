ce71c0737c2efe507614807bf095dcdf761b4ef9
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-02 16:34:27 -0600

    Update Route22.asm

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -1,16 +1,21 @@
 Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  2, NIDORAN_M
-	db  2, NIDORAN_F
-	db  3, MANKEY
-	db  3, RATTATA
-	db  4, NIDORAN_M
-	db  4, NIDORAN_F
+	db  5, SPEAROW
+	db  5, SPEAROW
 	db  5, MANKEY
-	db  2, SPEAROW
-	db  4, SPEAROW
-	db  6, SPEAROW
+	db  5, MANKEY
+	db  10, SPEAROW
+	db  10, MANKEY
+	db  5, RANDOM_CLASS_2
+	db  5, SQUIRTLE
+	db  10, SQUIRTLE
+	db  10, SQUIRTLE
 	end_grass_wildmons
+
+; Yellow Version:
+; MANKEY (10%)
+; SPEAROW (10%)
+; SQUIRTLE (5%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
