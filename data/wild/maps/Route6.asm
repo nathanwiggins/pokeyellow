7956dfb70d4e94a9bd898e334cbc38de2efcad60
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-02 13:54:02 -0600

    Update Route6.asm

--- data/wild/maps/Route6.asm
+++ data/wild/maps/Route6.asm
@@ -1,26 +1,38 @@
 Route6WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 15, PIDGEY
-	db 14, RATTATA
-	db  7, ABRA
-	db 16, PIDGEY
-	db 16, RATTATA
-	db 17, PIDGEY
-	db 17, PIDGEOTTO
-	db  3, JIGGLYPUFF
-	db  5, JIGGLYPUFF
-	db  7, JIGGLYPUFF
+	db  10, POLIWAG
+	db  10, POLIWAG
+	db  10, PSYDUCK
+	db  10, SLOWPOKE
+	db  10, PSYDUCK
+	db  10, SLOWPOKE
+	db  10, SEEL
+	db  10, KRABBY
+	db  10, KRABBY
+	db  10, KRABBY
 	end_grass_wildmons
 
+; Yellow Version:
+; POLIWAG (30%)
+; PSYDUCK (25%)
+; SLOWPOKE (25%)
+; SEEL (10%)
+; KRABBY (10%)
+
 	def_water_wildmons 3 ; encounter rate
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, GOLDUCK
-	db 20, GOLDUCK
+	db  10, MAGIKARP
+	db  10, MAGIKARP
+	db  10, MAGIKARP
+	db  10, HORSEA
+	db  10, HORSEA
+	db  10, GOLDEEN
+	db  10, GOLDEEN
+	db  10, GOLDEEN
+	db  10, MAGIKARP
+	db  10, MAGIKARP
 	end_water_wildmons
+
+; Yellow Version:
+; MAGIKARP (50%)
+; HORSEA (25%)
+; GOLDEEN (25%)
