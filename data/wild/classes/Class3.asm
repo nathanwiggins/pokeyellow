8783f9ed11218f8f79039ee7df43a590578071ad
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-02 16:28:05 -0600

    Update Class3.asm

--- data/wild/classes/Class3.asm
+++ data/wild/classes/Class3.asm
@@ -1,5 +1,15 @@
 RandomWildClass3:
-    db 3
-    db ODDISH
-    db BELLSPROUT
+    db 13
     db VULPIX
+    db GROWLITHE
+    db PONYTA
+    db SANDSHREW
+    db DIGLETT
+    db MACHOP
+    db GEODUDE
+    db CUBONE
+    db RHYHORN
+    db MAGNEMITE
+    db VOLTORB
+    db DROWZEE
+    db ABRA
