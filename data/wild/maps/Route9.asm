78a56cdad27295c526a9b3fb70e45d47f9fd76a3
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-02 13:46:50 -0600

    Update Route9.asm

--- data/wild/maps/Route9.asm
+++ data/wild/maps/Route9.asm
@@ -1,16 +1,21 @@
 Route9WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, NIDORAN_M
-	db 16, NIDORAN_F
-	db 18, RATTATA
-	db 18, NIDORAN_M
-	db 18, NIDORAN_F
-	db 17, SPEAROW
-	db 18, NIDORINO
-	db 18, NIDORINA
-	db 20, RATICATE
-	db 19, FEAROW
+	db  25, PIDGEOTTO
+	db  25, PIDGEOTTO
+	db  30, NIDORINA
+	db  30, NIDORINA
+	db  30, NIDORINO
+	db  30, NIDORINO
+	db  30, NIDORINO
+	db  30, NIDORINO
+	db  30, NIDORINA
+	db  30, NIDORINA
 	end_grass_wildmons
+
+; Yellow Version:
+; PIDGEOTTO (30%)
+; NIDORINA (35%)
+; NIDORINO (35%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
