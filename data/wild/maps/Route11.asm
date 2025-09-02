a5ab99d8b5192128d2195eeaeca8614ec1f80a13
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-02 13:48:58 -0600

    Update Route11.asm

--- data/wild/maps/Route11.asm
+++ data/wild/maps/Route11.asm
@@ -1,16 +1,20 @@
 Route11WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, PIDGEY
-	db 15, RATTATA
-	db 18, PIDGEY
-	db 15, DROWZEE
-	db 17, RATTATA
-	db 17, DROWZEE
-	db 18, PIDGEOTTO
-	db 20, PIDGEOTTO
-	db 19, DROWZEE
-	db 17, RATICATE
+	db  10, DROWZEE
+	db  10, DROWZEE
+	db  10, DROWZEE
+	db  10, DROWZEE
+	db  10, DROWZEE
+	db  20, EXEGGCUTE
+	db  20, EXEGGCUTE
+	db  20, EXEGGCUTE
+	db  20, EXEGGCUTE
+	db  20, EXEGGCUTE
 	end_grass_wildmons
+
+; Yellow Version:
+; DROWZEE (70%)
+; EXEGGCUTE (30%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
