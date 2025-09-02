1b32a82f2cf67d582518f3cc95804692a2c6f4a2
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-02 16:26:04 -0600

    Update Class1.asm

--- data/wild/classes/Class1.asm
+++ data/wild/classes/Class1.asm
@@ -1,5 +1,15 @@
 RandomWildClass1:
-    db 3
-    db BULBASAUR
-    db CHARMANDER
-    db SQUIRTLE
+    db 13
+    db ODDISH
+    db BELLSPROUT
+    db EXEGGCUTE
+    db PARAS
+    db VENONAT
+    db GRIMER
+    db KOFFING
+    db EKANS
+    db ZUBAT
+    db MEOWTH
+    db DODUO
+    db CLEFAIRY
+    db JIGGLYPUFF
