0a377d3716d7b42bbbd70c5ee842c310d0ece144
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-02 16:27:01 -0600

    Update Class2.asm

--- data/wild/classes/Class2.asm
+++ data/wild/classes/Class2.asm
@@ -1,5 +1,14 @@
 RandomWildClass2:
-    db 3
-    db PIDGEY
-    db RATTATA
-    db SPEAROW
+    db 12
+    db PSYDUCK
+    db POLIWAG
+    db TENTACOOL
+    db SLOWPOKE
+    db SEEL
+    db SHELLDER
+    db KRABBY
+    db HORSEA
+    db GOLDEEN
+    db STARYU
+    db MAGIKARP
+    db GASTLY
