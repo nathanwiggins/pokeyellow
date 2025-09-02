7ee2844f82a51a8445245273fb7fb8c45f4474d6
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-02 13:49:59 -0600

    Update DiglettsCave.asm

--- data/wild/maps/DiglettsCave.asm
+++ data/wild/maps/DiglettsCave.asm
@@ -1,16 +1,19 @@
 DiglettsCaveWildMons:
 	def_grass_wildmons 20 ; encounter rate
-	db 18, DIGLETT
-	db 19, DIGLETT
-	db 17, DIGLETT
-	db 20, DIGLETT
-	db 16, DIGLETT
-	db 15, DIGLETT
-	db 21, DIGLETT
-	db 22, DIGLETT
-	db 29, DUGTRIO
-	db 31, DUGTRIO
+	db  10, DIGLETT
+	db  10, DIGLETT
+	db  10, DIGLETT
+	db  10, DIGLETT
+	db  10, DIGLETT
+	db  10, DIGLETT
+	db  10, DIGLETT
+	db  10, DIGLETT
+	db  10, DIGLETT
+	db  10, DIGLETT
 	end_grass_wildmons
+
+; Yellow Version:
+; DIGLETT (100%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
