8d177e770c41cf21ed661208d8f7257034dfaf7c
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-03 15:54:51 -0600

    Update SafariZoneCenter.asm

--- data/wild/maps/SafariZoneCenter.asm
+++ data/wild/maps/SafariZoneCenter.asm
@@ -1,18 +1,28 @@
 SafariZoneCenterWildMons:
 	def_grass_wildmons 30 ; encounter rate
-	db 14, NIDORAN_M
-	db 36, NIDORAN_F
-	db 24, EXEGGCUTE
-	db 20, RHYHORN
-	db 23, NIDORINO
-	db 27, PARASECT
-	db 27, PARAS
-	db 32, PARASECT
-	db 22, TANGELA
-	db  7, CHANSEY
+	db 20, RANDOM_CLASS_5
+	db 20, RANDOM_CLASS_5
+	db 20, RANDOM_CLASS_5
+	db 30, RANDOM_CLASS_10
+	db 30, RANDOM_CLASS_10
+	db 40, RANDOM_CLASS_15
+	db 40, RANDOM_CLASS_15
+	db 50, RANDOM_CLASS_20
+	db 50, RANDOM_CLASS_20
+	db 50, RANDOM_CLASS_20
 	end_grass_wildmons
 
-	def_water_wildmons 0 ; encounter rate
+	def_water_wildmons 20 ; encounter rate
+	db 20, RANDOM_CLASS_4
+	db 20, RANDOM_CLASS_4
+	db 20, RANDOM_CLASS_4
+	db 30, RANDOM_CLASS_9
+	db 30, RANDOM_CLASS_9
+	db 40, RANDOM_CLASS_14
+	db 40, RANDOM_CLASS_14
+	db 50, RANDOM_CLASS_19
+	db 50, RANDOM_CLASS_19
+	db 50, RANDOM_CLASS_19
 	end_water_wildmons
 
 ;Land 5 10 15 20
