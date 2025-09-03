bac4ece1b5d436d9cbf666ffc709257f8dc558e8
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-03 14:39:38 -0600

    Create Class14.asm

---
+++ data/wild/classes/Class14.asm
@@ -0,0 +1,13 @@
+RandomWildClass14:
+    db 11
+		db GOLDUCK
+		db POLIWHIRL
+		db TENTACRUEL
+		db SLOWBRO
+		db DEWGONG
+		db SHELLDER
+		db KINGLER
+		db SEADRA
+		db SEAKING
+		db STARYU
+		db LAPRAS
