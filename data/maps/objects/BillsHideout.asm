eb1f9025bb370ba2178deb6a1a3aa46ee1327562
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-06 09:05:38 -0600

    Create BillsHideout.asm

---
+++ data/maps/objects/BillsHideout.asm
@@ -0,0 +1,12 @@
+BluesHouse_Object:
+	db $a ; border block
+
+	def_warp_events
+	warp_event  2,  7, LAST_MAP, 2
+	warp_event  3,  7, LAST_MAP, 2
+
+	def_bg_events
+
+	def_object_events
+
+	def_warps_to BLUES_HOUSE
