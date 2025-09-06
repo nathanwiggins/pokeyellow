10a54a9ce53cade6df2132a9bb3a393899d1e8c9
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-06 10:03:45 -0600

    Update BillsHideout.asm

--- text/BillsHideout.asm
+++ text/BillsHideout.asm
@@ -1,10 +1,10 @@
-BillsHideoutBillGreetingText:
+BillsHideoutBillGreetingText::
     text "Congrats on beating" 
     line "the POKEMON LEAGUE!"
     para "Care to battle me?"
     done
 
-BillsHideoutBillBattleStartText:
+BillsHideoutBillBattleStartText::
     text "Alright, let's do" 
     line "it then!"
     done
