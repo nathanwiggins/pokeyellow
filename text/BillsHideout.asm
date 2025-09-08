ecadf3b7f4f956cfab7ec3e6d1ed02170f37d905
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-08 14:55:55 -0600

    Update BillsHideout.asm

--- text/BillsHideout.asm
+++ text/BillsHideout.asm
@@ -1,8 +1,62 @@
 _BillsHideoutBillGreetingText::
-    text "Congrats on beating" 
-    line "the POKEMON LEAGUE!"
-    para "Care to battle me?"
-    done
+	text "Well now! You"
+	line "actually found"
+	cont "me here,"
+	cont "<PLAYER>!"
+
+	para "I gotta say,"
+	line "I'm impressed!"
+	cont "Though, I kinda"
+	cont "expected it too."
+
+	para "This place is"
+	line "RUIN VALLEY."
+
+	para "It's said that"
+	line "MEW, the ancestor"
+	cont "of all #MON,"
+	cont "dwells within"
+	cont "the #MON TEMPLE."
+
+	para "That's why I set"
+	line "up my hideout:"
+	cont "Hoping I'd find"
+	cont "it one day!"
+
+	para "But enough about"
+	line "me! Congrats on"
+	cont "beating the LEAGUE,"
+	cont "<PLAYER>!"
+
+	para "Y'know, the funny"
+	line "thing about"
+	cont "beating the LEAGUE"
+	cont "is..."
+
+	para "Suddenly, there's"
+	line "no real challenge"
+	cont "left!"
+
+	para "But never fear,"
+	line "<PLAYER>! I'm the"
+	cont "#MON COLLECTOR!"
+
+	para "I've got myself"
+	line "a collection of"
+	cont "the strongest"
+	cont "#MON around,"
+    cont "and I'm up for"
+    cont "a battle anytime!"
+
+	para "I can promise"
+	line "you that no two"
+	cont "battles will ever"
+	cont "be alike."
+
+	para "So, <PLAYER>, what"
+	line "do you say?"
+	cont "Want to battle?"
+	done
 
 _BillsHideoutBillBattleStartText::
     text "Alright, let's do" 
@@ -10,8 +64,8 @@ _BillsHideoutBillBattleStartText::
     done
 
 _BillsHideoutBillWinText::
-    text "Wow! You're"
-    line "really strong!"
+    text "Wow! Great"
+    line "job, <PLAYER>!"
     done
 
 _BillsHideoutBillLoseText::
